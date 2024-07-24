40e16b46a652f27311d55fb0214f4aa16713dedb
Daniel <McsColosPlay@gmail.com>
2024-07-24 17:31:43 +0300

    update routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://AzloRog.github.io/sls-app",
+  base: "https://AzloRog.github.io/sls-app#/",
   build: { chunkSizeWarningLimit: 1600 },
 });
