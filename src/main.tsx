51c83456e783beddfd0f35f37912d1889ebcdb81
Daniel <McsColosPlay@gmail.com>
2024-07-24 17:51:20 +0300

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createMemoryRouter,
-} from "react-router-dom";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
