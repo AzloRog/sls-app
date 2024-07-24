190d77f32b95a00d4a29af80c9010b3b04f93c80
Daniel <McsColosPlay@gmail.com>
2024-07-24 16:54:10 +0300

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {
-        path: "/",
+        path: "/sls-app",
         element: <HomePage />,
       },
       {
